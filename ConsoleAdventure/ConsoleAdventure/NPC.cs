dc80eeb913bc19961416f7cb3065cfd9f45d44a9
Joshua Steward <joshua.steward@students.fhu.edu>
2018-09-25 09:29:16 -0500

    meh

---
+++ ConsoleAdventure/ConsoleAdventure/NPC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAdventure
+{
+    class NPC : Character
+    {
+        public List<Interactable> MyStuff = new List<Interactable>();
+
+
+        public NPC(string name, char ico, ConsoleColor col, int hp) : base(name, ico, col, hp)
+        {
+        }
+    }
+}
